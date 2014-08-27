b30be96160ae0cb39e858cddd8d5bf1f2120e9a5
Lei <ld283@cornell.edu>
2014-08-27 03:44:14 -0700

    download files to experiment directory

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+DATA_DIR = '/home/lei/traffic/datasets/Phi'
+MATRIX_DIR = 'experiment_matrices'
+
+assert(os.path.isdir(DATA_DIR))
---
+++ download_matrices.py
@@ -0,0 +1,24 @@
+__author__ = 'lei'
+
+import requests
+import config
+import os
+
+def get_file(file_path, s3_file_name):
+    with open(file_path, 'wb') as handle:
+        response = requests.get('https://megacell.s3.amazonaws.com/'+s3_file_name, stream=True)
+
+        if not response.ok:
+            pass
+            # Something went wrong
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            handle.write(block)
+
+
+file_path = '/home/lei/traffic/experiment_matrices.7z'
+#get_file(file_path, 'experiment_matrices.7z')
+os.system('7za x '+file_path+'-aoa'+config.DATA_DIR)
